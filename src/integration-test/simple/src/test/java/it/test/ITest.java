f5067e87f9b019d9082e7fd655d4b34b2c2c6f34
alex079 <>
2022-06-28 02:37:59 +0200

    redo integration test with tricky cases

--- src/integration-test/simple/src/test/java/it/test/ITest.java
+++ src/integration-test/simple/src/test/java/it/test/ITest.java
@@ -1,15 +1,11 @@
 package it.test;
 
-import static java.util.Collections.singletonList;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 import static it.test.support.Utils.gsonConvert;
 import static it.test.support.Utils.jacksonConvert;
 
 import java.util.Objects;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
 
 import org.junit.jupiter.api.Test;
 
@@ -29,20 +25,84 @@ class ITest {
 	}
 
 	@Test
-	void testUnnamedQueryResult() throws JsonProcessingException {
-		String g = gsonConvert(it.gson.unnamedQuery.UnnamedQueryResult.builder()
-			.setItemsInDelivery(singletonList(it.gson.unnamedQuery.itemsInDelivery.ItemResult.builder()
-				.setPrice(1.0)
-				.build()
-			))
-			.build()
+	void testDocumentGeneration() {
+		it.gson.updateField2Mutation.UpdateField2Mutation gMutation =
+			new it.gson.updateField2Mutation.UpdateField2Mutation(var -> var.setId("2"));
+
+		assertThat(gMutation.getDocument()).contains(
+			"mutation updateField2($id: ID = \"\"\"0\"\"\") {\n" +
+				"\tfield2(arg1: $id) {\n" +
+				"\t\tid\n" +
+				"\t\tname\n" +
+				"\t\t...on Type2MutationField2 {\n" +
+				"\t\t\t...ValueFragment\n" +
+				"\t\t}\n" +
+				"\t\t...on Type1MutationField2 {\n" +
+				"\t\t\t...ValueFragment\n" +
+				"\t\t}\n" +
+				"\t}\n" +
+				"}",
+			"fragment ValueFragment on Type1MutationField2 {\n" +
+				"\tt1Value: value\n" +
+				"}",
+			"fragment ValueFragment on Type2MutationField2 {\n" +
+				"\tt2Value: value {\n" +
+				"\t\t...ValueFragment\n" +
+				"\t}\n" +
+				"}",
+			"fragment ValueFragment on Type3MutationField2 {\n" +
+				"\tt3Value: value\n" +
+				"}"
 		);
-		String j = jacksonConvert(new it.jackson.unnamedQuery.UnnamedQueryResult()
-			.setItemsInDelivery(singletonList(new it.jackson.unnamedQuery.itemsInDelivery.ItemResult()
-				.setPrice(1.0)
-			))
+
+		it.jackson.updateField2Mutation.UpdateField2Mutation jMutation =
+			new it.jackson.updateField2Mutation.UpdateField2Mutation(var -> var.setId("2"));
+
+		assertThat(jMutation.getDocument()).contains(
+			"mutation updateField2($id: ID = \"\"\"0\"\"\") {\n" +
+				"\tfield2(arg1: $id) {\n" +
+				"\t\tid\n" +
+				"\t\tname\n" +
+				"\t\t...on Type2MutationField2 {\n" +
+				"\t\t\t...ValueFragment\n" +
+				"\t\t}\n" +
+				"\t\t...on Type1MutationField2 {\n" +
+				"\t\t\t...ValueFragment\n" +
+				"\t\t}\n" +
+				"\t}\n" +
+				"}",
+			"fragment ValueFragment on Type1MutationField2 {\n" +
+				"\tt1Value: value\n" +
+				"}",
+			"fragment ValueFragment on Type2MutationField2 {\n" +
+				"\tt2Value: value {\n" +
+				"\t\t...ValueFragment\n" +
+				"\t}\n" +
+				"}",
+			"fragment ValueFragment on Type3MutationField2 {\n" +
+				"\tt3Value: value\n" +
+				"}"
 		);
-		assertThat(g).isEqualTo(j);
+	}
+
+	@Test
+	void testStringToResultConversion() {
+		final String resultExample = "{\n" +
+			"  \"field2\": {\n" +
+			"    \"id\": \"ID\",\n" +
+			"    \"name\": \"Name\",\n" +
+			"    \"t1Value\": 11,\n" +
+			"    \"t2Value\": {\n" +
+			"      \"t3Value\": 33\n" +
+			"    }\n" +
+			"  }\n" +
+			"}";
+
+		it.gson.updateField2Mutation.UpdateField2MutationResult gResult =
+			gsonConvert(resultExample, it.gson.updateField2Mutation.UpdateField2MutationResult.class);
+		it.jackson.updateField2Mutation.UpdateField2MutationResult jResult =
+			jacksonConvert(resultExample, it.jackson.updateField2Mutation.UpdateField2MutationResult.class);
+		assertThat(gResult.toString()).isEqualTo(jResult.toString());
 	}
 
 //	@Test
