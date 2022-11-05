679c6db50b5f39d20a5fc12a29f5972d1ba73004
alex079 <>
2022-11-05 13:53:36 +0100

    apply relay connection transformation to any definition

--- src/main/java/com/github/alme/graphql/generator/translator/RelayConnectionTranslator.java
+++ src/main/java/com/github/alme/graphql/generator/translator/RelayConnectionTranslator.java
@@ -6,16 +6,12 @@ import static java.util.stream.Collectors.toSet;
 import static com.github.alme.graphql.generator.translator.Util.fromFieldDef;
 
 import java.util.Collection;
-import java.util.List;
 
 import com.github.alme.graphql.generator.dto.GqlContext;
 import com.github.alme.graphql.generator.dto.GqlStructure;
 
 import graphql.kickstart.tools.relay.RelayConnectionFactory;
-import graphql.language.Definition;
-import graphql.language.Directive;
 import graphql.language.Document;
-import graphql.language.FieldDefinition;
 import graphql.language.ObjectTypeDefinition;
 import graphql.language.TypeName;
 
@@ -23,18 +19,8 @@ public class RelayConnectionTranslator implements Translator {
 
 	@Override
 	public void translate(Document doc, GqlContext ctx) {
-		List<ObjectTypeDefinition> definitions = doc.getDefinitionsOfType(ObjectTypeDefinition.class);
-		boolean connectionDirectiveIsFound = definitions.stream()
-			.map(ObjectTypeDefinition::getFieldDefinitions)
-			.flatMap(Collection::stream)
-			.map(FieldDefinition::getDirectives)
-			.flatMap(Collection::stream)
-			.map(Directive::getName)
-			.anyMatch("connection"::equals);
-		if (connectionDirectiveIsFound) {
-			List<? extends Definition<?>> injected = new RelayConnectionFactory().create(unmodifiableList(definitions));
-			populate(ctx, (Collection<ObjectTypeDefinition>) injected);
-		}
+		Collection<ObjectTypeDefinition> injected = new RelayConnectionFactory().create(unmodifiableList(doc.getDefinitions()));
+		populate(ctx, injected);
 	}
 
 	private void populate(GqlContext ctx, Collection<ObjectTypeDefinition> definitions) {
