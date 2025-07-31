13d2ab25ced1c06563c5a358c50211968c1183e1
Alex079 <>
2025-07-31 21:04:30 +0200

    remove unused import

--- src/main/java/com/github/alme/graphql/generator/io/translator/OperationTranslator.java
+++ src/main/java/com/github/alme/graphql/generator/io/translator/OperationTranslator.java
@@ -9,7 +9,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Objects;
